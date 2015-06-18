3fcba886d6eeafcc87b2aaf39ce718fb2ee92a7e
Unril <unril@live.ru>
2015-06-18 20:14:57 +0300

    This closes #1 Also changed standart library to default one.

--- STNucleoF401/main.cpp
+++ STNucleoF401/main.cpp
@@ -1,3 +1,4 @@
+
 #include "mbed.h"
 #include "FastIO.h"
 
@@ -28,45 +29,43 @@ static FastOut<D12> tickPin;
 struct Motor {
     template <size_t i, size_t edge>
     FORCE_INLINE static void writeStep(UIntConst<i>, UIntConst<edge>) {
-        __NOP();
-        __NOP();
+        s0Step = edge;
     }
 
     template <size_t i, size_t dir>
     FORCE_INLINE static void writeDirection(UIntConst<i>, UIntConst<dir>) {
-        __NOP();
-        __NOP();
+        s0Dir = dir;
     }
 
     // Stepper 0
 
-    FORCE_INLINE static void writeStep(UIntConst<0>, UIntConst<0>) { s0Step.clear(); }
+    FORCE_INLINE static void writeStep(UIntConst<1>, UIntConst<0>) { s0Step.clear(); }
 
-    FORCE_INLINE static void writeStep(UIntConst<0>, UIntConst<1>) { s0Step.set(); }
+    FORCE_INLINE static void writeStep(UIntConst<1>, UIntConst<1>) { s0Step.set(); }
 
-    FORCE_INLINE static void writeDirection(UIntConst<0>, UIntConst<0>) { s0Dir.clear(); }
+    FORCE_INLINE static void writeDirection(UIntConst<1>, UIntConst<0>) { s0Dir.clear(); }
 
-    FORCE_INLINE static void writeDirection(UIntConst<0>, UIntConst<1>) { s0Dir.set(); }
+    FORCE_INLINE static void writeDirection(UIntConst<1>, UIntConst<1>) { s0Dir.set(); }
 
     // Stepper 1
 
-    FORCE_INLINE static void writeStep(UIntConst<1>, UIntConst<0>) { s1Step.clear(); }
+    FORCE_INLINE static void writeStep(UIntConst<2>, UIntConst<0>) { s1Step.clear(); }
 
-    FORCE_INLINE static void writeStep(UIntConst<1>, UIntConst<1>) { s1Step.set(); }
+    FORCE_INLINE static void writeStep(UIntConst<2>, UIntConst<1>) { s1Step.set(); }
 
-    FORCE_INLINE static void writeDirection(UIntConst<1>, UIntConst<0>) { s1Dir.clear(); }
+    FORCE_INLINE static void writeDirection(UIntConst<2>, UIntConst<0>) { s1Dir.clear(); }
 
-    FORCE_INLINE static void writeDirection(UIntConst<1>, UIntConst<1>) { s1Dir.set(); }
+    FORCE_INLINE static void writeDirection(UIntConst<2>, UIntConst<1>) { s1Dir.set(); }
 
     // Stepper 2
 
-    FORCE_INLINE static void writeStep(UIntConst<2>, UIntConst<0>) { s2Step.clear(); }
+    FORCE_INLINE static void writeStep(UIntConst<3>, UIntConst<0>) { s2Step.clear(); }
 
-    FORCE_INLINE static void writeStep(UIntConst<2>, UIntConst<1>) { s2Step.set(); }
+    FORCE_INLINE static void writeStep(UIntConst<3>, UIntConst<1>) { s2Step.set(); }
 
-    FORCE_INLINE static void writeDirection(UIntConst<2>, UIntConst<0>) { s2Dir.clear(); }
+    FORCE_INLINE static void writeDirection(UIntConst<3>, UIntConst<0>) { s2Dir.clear(); }
 
-    FORCE_INLINE static void writeDirection(UIntConst<2>, UIntConst<1>) { s2Dir.set(); }
+    FORCE_INLINE static void writeDirection(UIntConst<3>, UIntConst<1>) { s2Dir.set(); }
 
     // Tick pin
 
@@ -99,10 +98,27 @@ struct Motor {
 static Motor motor;
 static Ticker ticker;
 
+int32_t getTicksPerSecond(size_t sz) {
+    switch (sz) {
+    case 1:
+    case 2:
+        return 160000;
+    case 3:
+    case 4:
+        return 140000;
+    case 5:
+    case 6:
+        return 120000;
+    default:
+        scAssert(!"Wrong axes count.");
+        return 0;
+    }
+}
+
+const size_t axesSize = 4;
+const int32_t ticksPerSecond = getTicksPerSecond(axesSize);
 const float Pi = 3.14159265358979323846f;
-const int32_t ticksPerSecond = 120000;
 const int32_t notifyPositionIntervalMs = 200;
-const size_t axesSize = 5;
 
 static SegmentsExecutor<axesSize, Motor, Ticker> executor(&motor, &ticker);
 static GCodeInterpreter<axesSize> interpreter(&executor);
@@ -123,19 +139,23 @@ int main() {
 
     interpreter.setTicksPerSecond(ticksPerSecond);
 
-    auto perRot = 1.f / (2.f * Pi);
-    interpreter.m102StepsPerUnitLengthOverride({1600.f / 5, 20 * 1600 * perRot, 10 * 1600 * perRot,
-                                                10 * 1600 * perRot, 10 * 1600 * perRot});
+    interpreter.m102StepsPerUnitLengthOverride(axConst<axesSize>(16000.f));
 
-    auto rotVel = 0.3f;
-    auto rotAcc = 0.1f;
-    interpreter.m100MaxVelocityOverride({30.f, rotVel, rotVel, rotVel, rotVel});
-    interpreter.m101MaxAccelerationOverride({20.f, rotAcc, rotAcc, rotAcc, rotAcc});
+    //    auto perRot = 1.f / (2.f * Pi);
+    //    interpreter.m102StepsPerUnitLengthOverride({20 * 1600 * perRot, 1600.f / 5, 20 * 1600 *
+    //    perRot,
+    //                                                10 * 1600 * perRot, 10 * 1600 * perRot});
+
+    //        auto rotVel = 0.5f;
+    //        auto rotAcc = 0.5f;
+    //        interpreter.m100MaxVelocityOverride({rotVel, 50.f , rotVel, rotVel, rotVel});
+    //        interpreter.m101MaxAccelerationOverride({rotAcc, 100.f, rotAcc, rotAcc, rotAcc});
 
     timer.start();
 
     while (true) {
         if (pc.readable()) {
+
             fgets(buffer, sizeof(buffer), pc);
             parser.parseLine(buffer);
         }
@@ -144,6 +164,7 @@ int main() {
             timer.reset();
 
             if (executor.isRunning() && !executor.isHoming()) {
+                printf("Move ");
                 axPrintf(interpreter.toUnits(executor.position()));
                 printf("\n");
             }
@@ -151,7 +172,7 @@ int main() {
 
         if (justStopped) {
             justStopped = false;
-
+            printf("Move ");
             axPrintf(interpreter.toUnits(executor.position()));
             printf("\nCompleted\n");
         }
